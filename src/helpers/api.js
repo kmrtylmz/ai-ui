4adebb62b0ec4928945aacc8c7417b38667cd5bf
Emre Kaşgür <40060075+emrekasg01@users.noreply.github.com>
2020-08-19 16:10:55 +0300

    Update api.js

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,11 +2,6 @@ import config from "../config";
 
 const API_VERSION = "v1";
 
-/**
- * Generate full url for muhsis internal api.
- * @param {*} url
- */
-
 export function generateApiEndpoint(url, server = "goApi") {
   return `${config.urls.api[server]}/${API_VERSION}/${url}`;
 }
